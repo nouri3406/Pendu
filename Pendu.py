b1fe2d1bb6bbf129acc6a042f5b30e0f12190d37
FOUDIL-BEY <nouri.foudil-bey@epitech.eu>
2025-09-20 19:44:31 +0200

    news pendu

--- Pendu.py
+++ Pendu.py
@@ -1,39 +1,55 @@
 import random
 from english_words import get_english_words_set
 
-
 def AKA ():
 	user =input ("enter your A.K.A !!! :" )
 	print (" Hooo yyyeeeaaah your A.K.A is : ", user )
 AKA()
 
 word =list (get_english_words_set(["gcide"]))
-enigme = random.choice (list(word)).lower()
+enigme = list( random.choice (list(word)).lower())
 caracter = len(enigme)
-
-def espace():
-	print("_"*caracter)
-espace()
-
+etat =["_"]*caracter 		
 fautes = 0
 limite = 12
-reponse =""*caracter
 
-while "_".join(reponse) != enigme and fautes < limite :
-	letter= input ("enter a letter : ").lower()
- 
-	if letter in enigme :	
-		for i in range (len(enigme)):
-			if enigme[i] == letter :
-				reponse[i] == letter
-			else :
-        			fautes += 1
-			print("heu...heu...Non !!")
-			print ("il te reste  :", limite - fautes, "vie" )
+def word_or_let (lettre) :
+	if len(lettre) > 1 :
+		word(lettre)
+	else :
+		let(lettre)
 
-		if fautes == limite:
-			print ("yoou loose ! play again", "le mot était :", enigme)
-			break	
-		if enigme == reponse:
-			print ("GG you a GOOAATTTT !!!!")
-			break
+def word (lettre) :
+	global etat, fautes, enigme, caracter
+	if list(lettre) == enigme :
+		etat = enigme
+	else :
+		fautes +=5
+
+def let (lettre) :
+	global etat, fautes, enigme, caracter 
+	if lettre in enigme :
+		for i, y in enumerate (enigme) :
+			if lettre == y :
+				etat [i]  = lettre
+	else :
+		fautes += 1
+
+while etat !=  enigme:
+	print("\nLe mot est :", " ".join(etat))
+	lettre = input("Entrer une lettre : ")
+	word_or_let(lettre)
+	lettres= []
+	if lettre not in lettres:
+        	lettres.append(lettre)    
+	print("La liste des lettres jouées :", lettres)
+	#word_or_let (lettre)
+		
+	if fautes >= limite:
+		print ("yoou loose ! play again", "le mot était :", enigme)
+		break
+	if etat == enigme:
+		print (*etat)		
+		print ("GG you a GOOAATTTT !!!!")
+		break
+	print (" il te reste : ", limite-fautes, "chance" )
