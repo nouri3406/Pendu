b0cef07237de5cd361e5ba83b6b059c09e4a378d
FOUDIL-BEY <nouri.foudil-bey@epitech.eu>
2025-09-21 20:01:58 +0200

    tabl

--- Pendu.py
+++ Pendu.py
@@ -5,7 +5,8 @@ import time
 def AKA ():
 	user =input ("enter your A.K.A !!! :" )
 	print (" Hooo yyyeeeaaah your A.K.A is : ", user )
-AKA()
+	return user
+user = AKA ()
 
 word =list (get_english_words_set(["gcide"]))
 enigme = list( random.choice (list(word)).lower())
@@ -15,6 +16,7 @@ lettres =[]
 fautes = 0
 limite = 12
 start =time.time()
+tableau_de_score = []
 
 def word_or_let (lettre) :
 	if len(lettre) > 1 :
@@ -38,6 +40,29 @@ def let (lettre) :
 	else :
 		fautes += 1
 
+def chrono () :
+	temps_écoulé = time.time()-start
+	minutes = int(temps_écoulé // 60)
+	secondes = int(temps_écoulé % 60)
+	centiemes = int((temps_écoulé * 100) % 100)
+	temps_str = f"{minutes:02d}:{secondes:02d}:{centiemes:02d}"
+	print ( "\nTemps écoulé : ", minutes, "m :", secondes,"s :", centiemes, "c" )
+	return temps_str
+
+def affichage () :
+	global user, enigme,tableau_de_score
+	temps_str = chrono ()
+	tableau_de_score.append({
+	"joueur": user,
+	"mot": "".join(enigme),
+	"temps": temps_str
+	})
+
+	print("\n=== TABLEAU DE SCORES ===")
+	for i, score in enumerate(tableau_de_score, 1):
+		print(f"{i}. AKA: {score['joueur']} -- Mot: {score['mot']} -- Temps: {score['temps']}")
+
+
 while etat !=  enigme:
 	print("\nLe mot est : ", *etat )
 	lettre = input("Entrer une lettre ou un mot : ")
@@ -45,17 +70,18 @@ while etat !=  enigme:
 		lettres.append( lettre )
 	word_or_let(lettre)
 
+
 	if fautes >= limite:
+		print (*etat)
 		print ("you loose ! play again", "le mot était :", *enigme)
+		affichage ()
 		break
 	if etat == enigme:
 		print (*etat)
 		print ("GG you a GOOAATTTT !!!!")
+		affichage ()
 		break
+
 	print (" il te reste : ", limite-fautes, "vies" )
 	print ("les lettres déja jouées sont : ", *lettres )
-	temps_écoulé = time.time()-start
-	minutes = int(temps_écoulé // 60)
-	secondes = int(temps_écoulé % 60)
-	centiemes = int((temps_écoulé * 100) % 100)
-	print ( "\nTemps écoulé : ", minutes, "m :", secondes,"s :", centiemes, "c" )
+
